424a442a7acd254d6312d0dee4df85813491fa38
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-22 13:42:38 -0500

    se creo  los estilos para la pantalla pricipal

--- Colegio/src/app/app-routing.module.ts
+++ Colegio/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PantallaInicioComponent } from './components/pantalla-inicio/pantalla-inicio.component';
+import { PantallaProfesorComponent} from './components/pantalla-profesor/pantalla-profesor.component';
+import { PantallaEstudianteComponent } from './components/pantalla-estudiante/pantalla-estudiante.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { MateriasComponent } from './components/materias/materias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Pantalla-inicio',component:PantallaInicioComponent},
+  {path: 'Pantalla-profesor',component:PantallaProfesorComponent},
+  {path: 'Pantalla-estudiante',component:PantallaEstudianteComponent},
+  {path: 'Materias',component:MateriasComponent},
+  {path: 'Tabla',component:TablaComponent},
+  {path:'**', redirectTo:'pantalla-inicio'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
