93af008f196d87962a4ab7be8be541f39d85c741
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-22 09:19:30 -0500

    se coencto el db con la base de datos

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql');
-
+const mysql = require('mysql2');
+ 
 const db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '1022933860',
-    database: 'escuela'
+    database: 'colegio'
 });
-
+ 
 db.connect(err => {
     if (err) {
         throw err;
     }
     console.log('Conectado a la base de datos MySQL');
 });
-
+ 
 module.exports = db;
